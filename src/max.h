3432b6ffa8bcd67e9b1960d9593d3ecfe3055d9f
hariharan235 <harig23@gmail.com>
2020-10-04 18:47:10 -0700

    add max function with limited coverage

---
+++ src/max.h
@@ -0,0 +1,14 @@
+/** \file max.h
+ *  Max header file
+ */
+#ifndef MAX_H
+#define MAX_H
+
+/*! \fn int max(int param1, int param2)
+    \brief Return 0,1 based on greater of params
+    \param param1
+    \param param2
+    \return 0 or 1
+ */
+int max(int param1, int param2);
+#endif // MAX_H
---
+++ test/test_max.c
@@ -0,0 +1,38 @@
+/*! \file test_max.c
+    \brief Tests on max.c
+ */
+
+// Includes
+#include "unity.h"
+#include "max.h"
+
+/*! \fn setUp(void)
+    \brief Executed before each test
+    \param None
+    \return None
+ */
+void setUp(void)
+{
+}
+
+/*! \fn tearDown(void)
+    \brief Executed at the end of each test
+    \param None
+    \return None
+ */
+void tearDown(void)
+{
+}
+
+/*! \fn test_max_param1_greaterthan_param2(void)
+    \brief Test to check if max returns 0 is param1 is greater than param2
+    \param None
+    \return None
+ */
+void test_max_param1_greaterthan_param2(void)
+{
+  int a = 17;
+  int b = 10;
+
+  TEST_ASSERT_EQUAL(0, max(a,b));
+}
