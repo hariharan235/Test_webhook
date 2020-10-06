32bc3c9315f681940c0b5294b90c6cfa99c3f5d2
hariharan235 <harig23@gmail.com>
2020-10-06 11:42:43 -0700

    Fix docs trial2

--- docs/sphinx/_build/html/search/functions_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['main',['main',['../main_8c.html#a840291bc02cba5474a4cb46a9b9566fe',1,'main(void):&#160;main.c'],['../main_8h.html#a840291bc02cba5474a4cb46a9b9566fe',1,'main(void):&#160;main.c']]],
-  ['max',['max',['../max_8c.html#a9514a57f256866c7e02113a8f2d41688',1,'max(int param1, int param2):&#160;max.c'],['../max_8h.html#a9514a57f256866c7e02113a8f2d41688',1,'max(int param1, int param2):&#160;max.c']]]
-];
